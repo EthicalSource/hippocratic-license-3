4da4285819c3702ce2415446df9f639ab6bc63fa
Nils Norman Haukås <mail@nilsnh.no>
2021-10-02 22:05:18 +0200

    :sparkles: start building license.html, and add commands for building all versions and clearing

--- scripts/licenseGenerator.js
+++ scripts/licenseGenerator.js
@@ -1,16 +1,16 @@
-import fs from 'fs'
+import { readFile, copyFile } from 'fs/promises'
 import path from 'path'
-import util from 'util'
 import combinations from 'combinations'
 import * as cheerio from 'cheerio'
+import rimraf from 'rimraf'
+import util from 'util'
 
 /**
  * loadBaseLicenseHTML loads the base HTML License file
  * @returns String HTML
  */
 export const loadBaseLicenseHTML = async () => {
-  const filePath = path.resolve('./content/version/3/0/license.md')
-  const readFile = util.promisify(fs.readFile)
+  const filePath = path.resolve('./content/version/3/0/license.html')
   return readFile(filePath, { encoding: 'utf-8' }).catch((err) => {
     console.log('Failed to read base license', err)
   })
@@ -35,7 +35,27 @@ export const findAvailableModules = (html) => {
   return modules
 }
 
-const buildOutVersions = ({ folderPath, versions = [] }) => {}
+const removeLicenseVersions = async () => {
+  const target = path.resolve('./content/version/3/0/HL-*')
+  const rm = util.promisify(rimraf)
+  return rm(target).catch((err) =>
+    console.error('Failed to delete license versions', err)
+  )
+}
+
+const buildOutVersions = async ({ versions = [] }) => {
+  const basePath = './content/version/3/0'
+  const source = path.resolve(basePath, 'license.html')
+  for (const version of versions) {
+    const destination = path.resolve(basePath, `HL-${version}.html`)
+    try {
+      await copyFile(source, destination)
+      console.log('Wrote:', path.basename(destination))
+    } catch (err) {
+      console.error('Failed to write file', err)
+    }
+  }
+}
 
 /**
  * getCombinations finds all possible module combinations.
@@ -43,7 +63,25 @@ const buildOutVersions = ({ folderPath, versions = [] }) => {}
  * @returns array of all possible module combinations.
  */
 export const getCombinations = (modules = []) => {
-  return combinations(modules).map((comb) => comb.join('-'))
+  return combinations(modules.map((m) => m.toUpperCase())).map((comb) =>
+    comb.join('-')
+  )
 }
 
-const main = () => {}
+const main = async () => {
+  console.log('Clearing license versions')
+  await removeLicenseVersions()
+  console.log('Generating license versions')
+  const text = await loadBaseLicenseHTML()
+  const modules = findAvailableModules(text)
+  const versions = getCombinations(modules)
+  await buildOutVersions({ versions })
+  console.log('Done with work')
+}
+
+if (process.env.NODE_ENV === 'clear') {
+  console.log('Clearing license versions')
+  await removeLicenseVersions()
+} else if (process.env.NODE_ENV !== 'test') {
+  main()
+}
