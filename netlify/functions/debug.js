5bc58a6bbfd5511d8366b60baea785181b3a60bb
Nils Norman Haukås <mail@nilsnh.no>
2021-11-29 00:22:36 +0100

    :sparkles: add Netlify functions debug url

---
+++ netlify/functions/debug.js
@@ -0,0 +1,38 @@
+const { builder } = require('@netlify/functions')
+
+/**
+ * Purpose: Expose URL for easily learning what data
+ * is available on the event and context objects.
+ *
+ * Example, vist this sub-url to get a debug page:
+ * /.netlify/functions/debug
+ *
+ * @param {*} event information about the inbound request
+ * @param {*} context overall execution context, like logged in user
+ * @returns
+ */
+async function debugHandler(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: `
+    <!DOCTYPE html>
+	    <html>
+		    <body>
+          <p>Event object:</p>
+		      <pre>
+            ${JSON.stringify(event, null, 2)}
+          </pre>
+          <p>Context object:</p>
+		      <pre>
+            ${JSON.stringify(context, null, 2)}
+          </pre>
+		    </body>
+    </html>
+    `,
+  }
+}
+
+exports.handler = builder(debugHandler)
