92d4475e9b9e2e33c17861b624c779940eab8165
Nils Norman Haukås <mail@nilsnh.no>
2021-11-28 17:01:44 +0100

    :construction: starting work on Netlify function for downloading configured license.

---
+++ netlify/functions/download-license.js
@@ -0,0 +1,23 @@
+const { builder } = require('@netlify/functions')
+
+async function downloadLicenseHandler(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: `
+    <!DOCTYPE html>
+	    <html>
+		    <body>
+          <p>Event object:</p>
+		      <pre>
+            ${JSON.stringify(event, null, 2)}
+          </pre>
+		    </body>
+    </html>
+    `,
+  }
+}
+
+exports.handler = builder(downloadLicenseHandler)
