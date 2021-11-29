96de3ddb754f5b97871b584b08400aa4598daa83
Nils Norman Haukås <mail@nilsnh.no>
2021-11-29 01:22:38 +0100

    refactor code

--- prepareLambdas.js
+++ prepareLambdas.js
@@ -2,9 +2,6 @@ import fs from 'fs/promises'
 import path from 'path'
 import { URL } from 'url'
 
-// Sourced from: https://stackoverflow.com/a/66651120
-const __dirname = new URL('.', import.meta.url).pathname
-
 /**
  * Purpose: The download-license.js handler needs to have
  * access to the full license text built by Hugo in order
@@ -15,6 +12,8 @@ const __dirname = new URL('.', import.meta.url).pathname
  * module that handles this process: https://github.com/netlify/zip-it-and-ship-it
  */
 const prepareLambdas = async () => {
+  // __dirname trick sourced from: https://stackoverflow.com/a/66651120
+  const __dirname = new URL('.', import.meta.url).pathname
   const src = path.resolve(__dirname, 'public/version/3/0/full/index.html')
   const dest = path.resolve(__dirname, 'netlify/functions/hl-full.json')
   const data = await fs.readFile(src, { encoding: 'utf-8' })
