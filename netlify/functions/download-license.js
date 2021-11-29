d9e93e4d17c10e43979c4bbe68f5ef67af4435c5
Nils Norman Haukås <mail@nilsnh.no>
2021-11-29 19:16:31 +0100

    redirect download-license from .netlify address to proxied /version/3/0 url

--- netlify/functions/download-license.js
+++ netlify/functions/download-license.js
@@ -2,6 +2,21 @@ const { builder } = require('@netlify/functions')
 const { licenseHTML } = require('./hl-full.json')
 
 async function downloadLicenseHandler(event, context) {
+  // Redirect if wrong sub-path
+  if (event.path.startsWith('/.netlify/functions')) {
+    return {
+      statusCode: 302,
+      headers: {
+        Location: '/version/3/0/full',
+      },
+    }
+  }
+  // Redirect if uppercase and/or wrong order of modules.
+
+  // Parse url and provide correct modules to activate.
+
+  // Return proper format depending on file extension.
+
   return {
     statusCode: 200,
     headers: {
