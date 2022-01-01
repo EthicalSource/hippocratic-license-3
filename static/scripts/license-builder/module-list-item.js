7e91806d82c0418cb76869e52f590bcdc61e0f99
Nils Norman Haukås <nilsnh@met.no>
2022-01-01 22:50:15 +0100

    WIP: start work on a better button style

--- static/scripts/license-builder/module-list-item.js
+++ static/scripts/license-builder/module-list-item.js
@@ -60,11 +60,14 @@ export class ModuleListItem extends HTMLElement {
     this.checkbox.type = 'checkbox'
     this.checkboxLabel = cr('label')
     this.findButton = cr('button')
+    this.findButton.setAttribute('part', 'button')
+    
     this.findButton.innerHTML = `<svg title="" xmlns="http://www.w3.org/2000/svg" class="" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
     </svg>`
 
     this.infoButton = cr('button')
+    this.infoButton.setAttribute('part', 'button')
     this.infoButton.innerHTML = `<svg title="" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20">
     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
     </svg>`
