781d20ef5efad3e0d326e99ecc0f70327526bee7
Nils Norman Haukås <nilsnh@met.no>
2022-01-02 19:17:07 +0100

    :sparkles: set fill:currentColor to fix black icon.

--- static/scripts/license-builder/module-list-item.js
+++ static/scripts/license-builder/module-list-item.js
@@ -68,7 +68,7 @@ export class ModuleListItem extends HTMLElement {
 
     this.infoButton = cr('button')
     this.infoButton.setAttribute('part', 'button')
-    this.infoButton.innerHTML = `<svg part="button-icon" title="" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20">
+    this.infoButton.innerHTML = `<svg part="button-icon" style="fill:currentColor;" title="" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20">
     <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
     </svg>`
     this.infoButton.setAttribute('part', 'button')
